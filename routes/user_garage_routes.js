ee29e0841935a017562903b78db2a2f1817bb20c
Edgars <edgarsapinis1@gmail.com>
2023-12-28 17:18:11 +0100

    Added routes for viewing car, also for like button functinality when viewing car

--- routes/user_garage_routes.js
+++ routes/user_garage_routes.js
@@ -1,26 +1,27 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const cars = express.Router();
-const db = require('../db/db.js');
-const jwt = require('jsonwebtoken');
+const db = require("../db/db.js");
+const jwt = require("jsonwebtoken");
 
 // Middleware to verify the JWT token
 function verifyToken(req, res, next) {
-  const token = req.headers['authorization'];
-  console.log('token get_user ', token);
-
+  const token = req.headers["authorization"];
+  console.log("token get_user ", token);
 
   if (!token) {
-    return res.status(401).json({ success: false, error: 'No token provided' });
+    return res.status(401).json({ success: false, error: "No token provided" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      console.error('Failed to authenticate token:', err);
-      return res.status(401).json({ success: false, error: 'Failed to authenticate token' });
+      console.error("Failed to authenticate token:", err);
+      return res
+        .status(401)
+        .json({ success: false, error: "Failed to authenticate token" });
     }
 
-    console.log('Decoded token:', decoded);
+    console.log("Decoded token:", decoded);
 
     req.userId = decoded.userId; // Update to match the payload property name
     next();
@@ -28,15 +29,17 @@ function verifyToken(req, res, next) {
 }
 
 // Gets all the cars of the user and comment counts with the given username
-cars.get('/user_cars', verifyToken, (req, res, next) => {
+cars.get("/user_cars", verifyToken, (req, res, next) => {
   const providedUsername = req.query.username;
 
   if (!providedUsername) {
-    return res.status(400).json({ success: false, error: 'Username is required in the query parameters' });
+    return res.status(400).json({
+      success: false,
+      error: "Username is required in the query parameters",
+    });
   }
 
-  const query =
-  `SELECT
+  const query = `SELECT
     c.*,
     ci.car_image_url AS first_photo,
     COALESCE(cmt.comment_count, 0) AS comment_count
@@ -63,131 +66,185 @@ cars.get('/user_cars', verifyToken, (req, res, next) => {
         car_id
   ) cmt ON c.id_car = cmt.car_id
   WHERE
-    u.username = ?`
+    u.username = ?`;
 
   db.query(query, [providedUsername], (err, results) => {
     if (err) {
-      console.error('Error querying database: ', err);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
+      console.error("Error querying database: ", err);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
     } else {
       res.status(200).json({ success: true, cars: results });
     }
   });
-})
+});
+
+// Gets the car with the given car ID
+cars.get("/get_car", verifyToken, (req, res, next) => {
+  const userId = req.userId;
+  const carId = req.query.car_id;
+
+  const query = `
+    SELECT
+      Car.*,
+      (SELECT COUNT(car_id) FROM Likes WHERE car_id = Car.id_car) AS likes_count,
+      (SELECT COUNT(*) FROM Likes WHERE user_id = ? AND car_id = Car.id_car) AS user_like_count
+    FROM
+      Car
+    WHERE
+      id_car = ?
+  `;
+
+  db.query(query, [userId, carId], (err, results) => {
+    if (err) {
+      console.error("Error querying database: ", err);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    } else if (results.length === 0) {
+      res.status(404).json({ success: false, error: "Car not found" });
+    } else {
+      const carData = results[0];
+      const hasLiked = carData.user_like_count > 0;
+
+      res.json({ success: true, car: carData, hasLiked: hasLiked });
+    }
+  });
+});
 
 // Adds car to the database and returns the car ID
-cars.post('/add_car', verifyToken, (req, res) => {
+cars.post("/add_car", verifyToken, (req, res) => {
   const userId = req.userId;
   const { make_model, desc_car, year } = req.body;
 
-  const insertQuery = 'INSERT INTO Car (user_id, make_model, desc_car, year) VALUES (?, ?, ?, ?)';
-  db.query(insertQuery, [userId, make_model, desc_car, year], (err, results) => {
-    if (err) {
-      console.error('Error querying database: ', err);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
-    } else {
-      // Access the last inserted ID using results.insertId
-      const insertedId = results.insertId;
-      res.json({ success: true, insertedId });
+  const insertQuery =
+    "INSERT INTO Car (user_id, make_model, desc_car, year) VALUES (?, ?, ?, ?)";
+  db.query(
+    insertQuery,
+    [userId, make_model, desc_car, year],
+    (err, results) => {
+      if (err) {
+        console.error("Error querying database: ", err);
+        res
+          .status(500)
+          .json({ success: false, error: "Internal Server Error" });
+      } else {
+        // Access the last inserted ID using results.insertId
+        const insertedId = results.insertId;
+        res.json({ success: true, insertedId });
+      }
     }
-  });
+  );
 });
 
 // Updates the car
-cars.post('/update_car', verifyToken, (req, res) => {
+cars.post("/update_car", verifyToken, (req, res) => {
   // const userId = req.userId;
   const { make_model, desc_car, year, id_car } = req.body;
 
-  const updateQuery = 'UPDATE Car SET make_model = ?, desc_car = ?, year = ? WHERE id_car = ?';
-  db.query(updateQuery, [make_model, desc_car, year, id_car], (err, results) => {
-    if (err) {
-      console.error('Error querying database: ', err);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
-    } else {
-      res.json({ success: true, results });
+  const updateQuery =
+    "UPDATE Car SET make_model = ?, desc_car = ?, year = ? WHERE id_car = ?";
+  db.query(
+    updateQuery,
+    [make_model, desc_car, year, id_car],
+    (err, results) => {
+      if (err) {
+        console.error("Error querying database: ", err);
+        res
+          .status(500)
+          .json({ success: false, error: "Internal Server Error" });
+      } else {
+        res.json({ success: true, results });
+      }
     }
-  });
+  );
 });
 
 // Deletes the car
-cars.post('/delete_car', verifyToken, (req, res) => {
+cars.post("/delete_car", verifyToken, (req, res) => {
   // const userId = req.userId;
   const { id_car } = req.body;
 
-  const deleteQuery = 'DELETE FROM Car WHERE id_car = ?';
+  const deleteQuery = "DELETE FROM Car WHERE id_car = ?";
   db.query(deleteQuery, [id_car], (err, results) => {
     if (err) {
-      console.error('Error querying database: ', err);
-      res.status(500).json({ success: false, error: 'Internal server error' });
+      console.error("Error querying database: ", err);
+      res.status(500).json({ success: false, error: "Internal server error" });
     } else {
       res.json({ success: true, results });
     }
   });
 });
 
+// Likes the car
+cars.post("/like_car", verifyToken, (req, res) => {
+  const userId = req.userId;
+  const { car_id } = req.body;
+
+  const insertQuery = "INSERT INTO Likes (user_id, car_id) VALUES (?, ?)";
+  db.query(insertQuery, [userId, car_id], (err, results) => {
+    if (err) {
+      console.error("Error querying database: ", err);
+      res.status(500).json({ success: false, error: "Internal server error" });
+    } else {
+      res.json({ success: true, results });
+    }
+  });
+});
 
 // Gets all car images of the car with the given car ID
-cars.get('/car_images', verifyToken, (req, res, next) => {
+cars.get("/car_images", verifyToken, (req, res, next) => {
   const userId = req.userId;
   // get car_id from url
   const car_id = req.query.car_id;
 
   if (!car_id) {
-    return res.status(400).json({ success: false, error: 'Car ID is required in the query parameters' });
+    return res.status(400).json({
+      success: false,
+      error: "Car ID is required in the query parameters",
+    });
   }
 
-  const query = 'SELECT * FROM CarImage WHERE car_id = ?';
+  const query = "SELECT * FROM CarImage WHERE car_id = ?";
 
   db.query(query, [car_id], (err, results) => {
     if (err) {
-      console.error('Error querying database: ', err);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
-    }
-    else {
+      console.error("Error querying database: ", err);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    } else {
       res.json({ success: true, results });
     }
   });
 });
 
-
 // Adds car image to the database
-cars.post('/add_car_image', verifyToken, (req, res) => {
+cars.post("/add_car_image", verifyToken, (req, res) => {
   const userId = req.userId;
   const { car_id, car_image_url } = req.body;
 
-  const insertQuery = 'INSERT INTO CarImage (car_id, car_image_url) VALUES (?, ?)';
+  const insertQuery =
+    "INSERT INTO CarImage (car_id, car_image_url) VALUES (?, ?)";
   db.query(insertQuery, [car_id, car_image_url], (err, results) => {
     if (err) {
-      console.error('Error querying database: ', err);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
-    }
-    else {
+      console.error("Error querying database: ", err);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    } else {
       res.json({ success: true, results });
     }
   });
 });
 
 // Deletes car image from the database
-cars.post('/delete_car_image', verifyToken, (req, res) => {
+cars.post("/delete_car_image", verifyToken, (req, res) => {
   const userId = req.userId;
   const { id_car_image } = req.body;
 
-  const deleteQuery = 'DELETE FROM CarImage WHERE id_car_image = ?';
+  const deleteQuery = "DELETE FROM CarImage WHERE id_car_image = ?";
   db.query(deleteQuery, [id_car_image], (err, results) => {
     if (err) {
-      console.error('Error querying database: ', err);
-      res.status(500).json({ success: false, error: 'Internal server error' });
-    }
-    else {
+      console.error("Error querying database: ", err);
+      res.status(500).json({ success: false, error: "Internal server error" });
+    } else {
       res.json({ success: true, results });
     }
   });
 });
-
-
-
-
-
-
 
 module.exports = cars;
