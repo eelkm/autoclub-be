f96aa883f036bd67cf47ac69ca803042dacbd9be
Edgars <edgarsapinis1@gmail.com>
2024-01-11 10:30:26 +0100

    Moved verifytoken middleware into seperate file

--- routes/event_routes.js
+++ routes/event_routes.js
@@ -2,31 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const events = express.Router();
 const { executeQuery } = require("../db/db.js");
+const { verifyToken } = require("../verifytoken.js");
 const jwt = require("jsonwebtoken");
-
-// Middleware to verify the JWT token
-function verifyToken(req, res, next) {
-  const token = req.headers["authorization"];
-  console.log("token get_user ", token);
-
-  if (!token) {
-    return res.status(401).json({ success: false, error: "No token provided" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      console.error("Failed to authenticate token:", err);
-      return res
-        .status(401)
-        .json({ success: false, error: "Failed to authenticate token" });
-    }
-
-    console.log("Decoded token:", decoded);
-
-    req.userId = decoded.userId; // Update to match the payload property name
-    next();
-  });
-}
 
 // Gets all events
 events.get("/get_events", verifyToken, async (req, res, next) => {
--- routes/user_posts_routes.js
+++ routes/user_posts_routes.js
@@ -2,31 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const post_user = express.Router();
 const { executeQuery } = require("../db/db.js");
+const { verifyToken } = require("../verifytoken.js");
 const jwt = require("jsonwebtoken");
-
-// Middleware to verify the JWT token
-function verifyToken(req, res, next) {
-  const token = req.headers["authorization"];
-  console.log("token get_user ", token);
-
-  if (!token) {
-    return res.status(401).json({ success: false, error: "No token provided" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      console.error("Failed to authenticate token:", err);
-      return res
-        .status(401)
-        .json({ success: false, error: "Failed to authenticate token" });
-    }
-
-    console.log("Decoded token:", decoded);
-
-    req.userId = decoded.userId; // Update to match the payload property name
-    next();
-  });
-}
 
 // Gets the posts of the user with comment count with the given username
 post_user.get("/user_posts", verifyToken, async (req, res, next) => {
